ac8e006f5852ed4033884553f2fd94c73c50ed67
dcrivard <156547929+dcrivard@users.noreply.github.com>
2025-01-27 17:39:57 -0500

    Saved

--- week 3/lab3.py
+++ week 3/lab3.py
@@ -13,7 +13,7 @@ ages = []
 registered = []
 votes = []
 
-with open("week 3\class_grades.csv") as file:
+with open("week 3/voters_202040.csv") as file:
     reader = csv.reader(file)
     next(reader) 
     
@@ -21,7 +21,13 @@ with open("week 3\class_grades.csv") as file:
         id_numbers.append(row[0])  
         ages.append(row[1]) 
         registered.append(row[2])  
-        votes.append(row[3])       
+        votes.append(row[3])    
 
 
 
+
+print(f"\nNumber of individuals not eligible to register: {registered}")
+print(f"\nNumber of individuals who are old enough to vote but have not registered.")
+print(f"\nNumber of individuals who are eligible to vote but did not vote.")
+print(f"\nNumber of individuals who did vote.")
+print(f"\nNumber of records processed.")
