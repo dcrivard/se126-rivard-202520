b60cf76fb784f66abff1831f64c490f79c1ca1f0
dcrivard <156547929+dcrivard@users.noreply.github.com>
2025-02-24 15:11:22 -0500

    lab5

--- Week 6/lab5.py
+++ Week 6/lab5.py
@@ -48,3 +48,195 @@ with open("Week 6/book_list.csv") as csvfile:
 for i in range(0, len(library_num)):
     print(f"{library_num[i]:5}  {title[i]:25}  {author[i]:15}  {genre[i]:20}  {page[i]:5}  {status[i]:25}")
 print("--------------------------------------------------------------------------------")
+#each title shown in order by bubble sort
+for i in range(0, len(title) -1):
+
+
+    for index in range(0, len(title) - 1):#inner loop
+
+
+        #below if statement determines the sort
+
+        #list used is the list being sorted
+
+        # > is for increasing order, < for decreasing
+
+        if(title[index] > title[index + 1]):
+
+            print("\t\t SWAP! ", title[index], "<-->", title[index + 1])
+
+            #if above is true, swap places!
+
+            temp = title[index]
+
+            title[index] = title[index + 1]
+
+            title[index + 1] = temp
+
+            #swap all other values
+
+            temp = author[index]
+
+            author[index] = author[index + 1]
+
+            author[index + 1] = temp
+            #swap library num
+            temp = library_num[index]
+
+            library_num[index] = library_num[index + 1]
+
+            library_num[index + 1] = temp
+            #swap genre
+            temp = genre[index]
+
+            genre[index] = genre[index + 1]
+
+            genre[index + 1] = temp
+            #swap page
+            temp = page[index]
+
+            page[index] = page[index + 1]
+
+            page[index + 1] = temp
+            #swap status
+            temp = status[index]
+
+            status[index] = status[index + 1]
+
+            status[index + 1] = temp
+
+#disconnected from file
+ans = 'y'
+
+while ans == "y":
+    print("\tSEARCHING MENU")
+    print("1. Search by title") #searches by title
+    print("2. Search by author") #binary search review
+    print("3. Search by genre") #find part of a whole
+    print("4. Search by Library number")
+    print("5. EXIT")
+
+    search_type = input("\nHow would you like to search today? [1-5]: ")
+    if search_type == 5:
+        print("\n\nThank you for using my program, GOODBYE!\n\n\n")
+    #if the search type is wrong
+    if search_type not in ["1", "2", "3", "4", "5"]:
+         print("***INVALID ENTRY!***\nPlease try again")
+
+#Option #1
+    elif search_type == "1":
+        print('search by title or title key word')
+
+        search = input("Enter the Title you are looking for: ")
+
+        min = 0
+        max = len(title) - 1
+        mid = int((min + max)) / 2
+
+        while min < max and search.lower() !=  title[mid].lower():
+            if search.lower() < title[mid].lower():
+                max = mid - 1
+            else:
+                min = mid + 1
+
+            mid = int((min + max)) / 2
+
+        if search.lower() == title[mid].lower():
+            print(f"we found your search for {search}, details below: ")
+            print(f"{'TITLE':12}  {'AUTHOR':3}  {'GENRE'}")
+            print(f"------------------------------------------------------")
+            print(f"{title[mid]:12}  {author[mid]:3}  {genre[mid]}")
+            print(f"---------------------------------------------------------")
+        else:
+            print(f"\nYour search for {search} is complete, no matches found ")
+
+    elif search_type == "2": #search by author
+        print("\n~Search by author~")
+        found = []  #empty list, found locations (index) will be stored if/when found
+        search_let= input("Enter the author you wish to find: ") #genre we are looking through all students for
+
+        #step 2: perform search algo (seq. search -> for loop w/ if statement)
+        for i in range(0, len(author)):
+            #for loop performs the SEQUENCE - from start through end of list items
+
+            if search_let.upper() == author[i]: 
+                #if performs the SEARCH - is what we're looking for here in the list?
+                found.append(i)  #stores found item's INDEX LOCATION to the found list because we may have multiple students whose letter grade fits the searched for grade
+                print(f"Found a {search_let} author in INDEX {i}")
+
+        #step 3: display results to user; make sure you give info: both for found or NOT found
+        if not found: #'if not found' means 'found' is an EMPTY LIST
+            #NOT found
+            print(f"Your search for {search_let} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+        else: 
+            #last name FOUND!
+            print(f"Your search for {search_let} was FOUND! Here is their data: ")
+
+            #'found' is a list populated with index locations - we loop through this list, and use found[i] (which again, holds an INDEX from our other searched-through list) to be recalled and used below
+            for i in range(0, len(found)):
+                print(f"{library_num[i]:5}  {title[i]:25}  {author[i]:15}  {genre[i]:20}  {page[i]:5}  {status[i]:25}")
+
+    elif search_type == "3": #search by genre
+        print("\n~Search by genre~")
+        found = []  #empty list, found locations (index) will be stored if/when found
+        search_let= input("Enter the Genre you wish to find: ") #genre we are looking through all students for
+
+        #step 2: perform search algo (seq. search -> for loop w/ if statement)
+        for i in range(0, len(genre)):
+            #for loop performs the SEQUENCE - from start through end of list items
+
+            if search_let.upper() == genre[i]: 
+                #if performs the SEARCH - is what we're looking for here in the list?
+                found.append(i)  #stores found item's INDEX LOCATION to the found list because we may have multiple students whose letter grade fits the searched for grade
+                print(f"Found a {search_let} gebre in INDEX {i}")
+
+        #step 3: display results to user; make sure you give info: both for found or NOT found
+        if not found: #'if not found' means 'found' is an EMPTY LIST
+            #NOT found
+            print(f"Your search for {search_let} was NOT FOUND!")
+            print("Check your cAsInG and sPeLlInG and try again!")
+        else: 
+            #last name FOUND!
+            print(f"Your search for {search_let} was FOUND! Here is their data: ")
+
+            #'found' is a list populated with index locations - we loop through this list, and use found[i] (which again, holds an INDEX from our other searched-through list) to be recalled and used below
+            for i in range(0, len(found)):
+                print(f"{library_num[i]:5}  {title[i]:25}  {author[i]:15}  {genre[i]:20}  {page[i]:5}  {status[i]:25}")
+    elif search_type == "4": #search for library number
+        search = input("\nWhat Library number are you searching for?:  ")
+        for i in range(0, len(library_num)):
+            if library_num[i] == {search}:
+                print(f"{library_num[i]:5}  {title[i]:25}  {author[i]:15}  {genre[i]:20}  {page[i]:5}  {status[i]:25}")
+                print("-------------------------------------------------------------------------------------------")
+
+        else:
+            if i == len(library_num):
+                print("search not found")
+
+    elif search_type == "4":
+        print("\n~Search by status~")
+        found = []  #empty list, found locations (index) will be stored if/when found
+        search_let= input("Enter the status you wish to find: ") #genre we are looking through all students for
+
+        #step 2: perform search algo (seq. search -> for loop w/ if statement)
+        for i in range(0, len(status)):
+            #for loop performs the SEQUENCE - from start through end of list items
+
+            if status[i] == 'avaliable': 
+                #if performs the SEARCH - is what we're looking for here in the list?
+                found.append(i)  #stores found item's INDEX LOCATION to the found list because we may have multiple students whose letter grade fits the searched for grade
+                print(f"Found a {search_let} status in INDEX {i}")
+                i + 1
+        else: 
+            #last name FOUND!
+            print(f"Your search for {search_let} was FOUND! Here is their data: ")
+
+            #'found' is a list populated with index locations - we loop through this list, and use found[i] (which again, holds an INDEX from our other searched-through list) to be recalled and used below
+            for i in range(0, len(found)):
+                print(f"{library_num[i]:5}  {title[i]:25}  {author[i]:15}  {genre[i]:20}  {page[i]:5}  {status[i]:25}")
+
+
+    elif search_type == "5":
+        print(f"\nYou have chosen to EXIT")
+        ans = "N"
